6b3571d25d72643f62960e5244ca70f35aaf2419
Seyrit <seyrit16@mail.ru>
2025-08-30 14:49:31 +0300

    созданы репозитории

---
+++ src/main/java/com/example/library/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.example.library.repository;
+
+import com.example.book_library.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+}
